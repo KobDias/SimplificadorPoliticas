2318b60365ec34af7ed8bce8617978e2341fd9f2
KobDias <sofhia.dias@etec.sp.gov.br>
2025-04-20 11:19:18 -0300

    Add models e db

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from db import db
+
+class Politica(db.Model):
+    __tablename__ = 'politicas'
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(80), nullable=False)
+    descricao = db.Column(db.String(200), nullable=False)
+    data_criacao = db.Column(db.DateTime, nullable=False)
+    
